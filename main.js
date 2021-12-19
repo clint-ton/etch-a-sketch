44cfb239133df17e37f1c48b4183c8c59f200edb
clinton <cjw237@uclive.ac.nz>
2021-12-19 22:24:09 +1300

    Dimension slider and tidy up

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
-function makeGrid (size) {
-    for (let i = 0; i < size; i++) {
+// DOM Handle definitions
+
+const slider = document.querySelector('#dimension');
+const container = document.querySelector('#container');
+const clearBtn = document.querySelector('#clear-btn');
+
+// Function Defintions
+
+function makeGrid () {
+
+    // Deletes all divs from canvas
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    };
+
+    // Loops creating div grid based off slider value
+    for (let i = 0; i < slider.value; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
-        for (let i = 0; i < size; i++) {
+        for (let i = 0; i < slider.value; i++) {
             const div = document.createElement('div');
             div.classList.add('box');
             row.appendChild(div);
         }
         container.appendChild(row);
     }
+    
+    // adds a mouse over event listener to every div
+    nodeArray = Array.from(document.querySelectorAll('.box'));
+    nodeArray.forEach(addMouseOver);
+
 }
 
 function addMouseOver(node) {
     node.addEventListener('mouseover', function() {
+        // Adds a css class with a different color on mouseover
         node.classList.add('coloured');
     })
 }
@@ -21,17 +42,10 @@ function clearCell (node) {
     node.classList.remove('colored');
 }
 
-const container = document.querySelector('#container');
+// Add event listeners (canvas event listeners in makeGrid)
+clearBtn.addEventListener('click', makeGrid);
+slider.addEventListener('input', makeGrid);
 
-const clearBtn = document.querySelector('#clear-btn');
+// Draw inital canvas
+makeGrid();
 
-clearBtn.addEventListener('click', function() {
-    location.reload();
-});
-
-
-makeGrid(16);
-
-nodeArray = Array.from(document.querySelectorAll('.box'));
-
-nodeArray.forEach(addMouseOver);
