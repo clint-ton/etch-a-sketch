2f82924fe6aea8f2d740501f4e4068ca9a2c6f88
clinton <cjw237@uclive.ac.nz>
2021-12-19 22:43:00 +1300

    rainbow button

--- main.js
+++ main.js
@@ -3,7 +3,13 @@
 const slider = document.querySelector('#dimension');
 const container = document.querySelector('#container');
 const clearBtn = document.querySelector('#clear-btn');
+const rainbowBtn = document.querySelector('#rainbow-btn');
 
+
+// flags for modes
+let rainbow = false;
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indgo', 'violet']
+let colorIndex = 0;
 // Function Defintions
 
 function makeGrid () {
@@ -27,7 +33,11 @@ function makeGrid () {
     
     // adds a mouse over event listener to every div
     nodeArray = Array.from(document.querySelectorAll('.box'));
-    nodeArray.forEach(addMouseOver);
+    if (rainbow) {
+        nodeArray.forEach(addMouseOverRainbow);
+    } else {
+        nodeArray.forEach(addMouseOver);
+    }
 
 }
 
@@ -38,13 +48,31 @@ function addMouseOver(node) {
     })
 }
 
+function addMouseOverRainbow(node) {
+    node.addEventListener('mouseover', function() {
+        node.style.cssText = `background-color: ${colors[colorIndex++ % colors.length]}`;
+    })
+}
+
 function clearCell (node) {
     node.classList.remove('colored');
+}
+
+function setRainbow () {
+    rainbow = !rainbow;
+    if (rainbow) {
+        rainbowBtn.classList.add('rainbow');
+    } else {
+        rainbowBtn.classList.remove('rainbow');
+    }
+    makeGrid()
 }
 
 // Add event listeners (canvas event listeners in makeGrid)
 clearBtn.addEventListener('click', makeGrid);
 slider.addEventListener('input', makeGrid);
+rainbowBtn.addEventListener('click', setRainbow);
+
 
 // Draw inital canvas
 makeGrid();
